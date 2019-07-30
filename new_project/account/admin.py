3974294b3d0835f623ae70a7aba5de7017708da3
nxmastic <48348520+nxmastic@users.noreply.github.com>
2019-07-30 11:46:16 +0900

    conflict admin fix

--- new_project/account/admin.py
+++ new_project/account/admin.py
@@ -1,12 +1,5 @@
-<<<<<<< HEAD
 from django.contrib import admin
 from .models import Account
 # Register your models here.
 
-=======
-from django.contrib import admin
-from .models import Account
-# Register your models here.
-
->>>>>>> 2480c54b8f7df51e28a370924a66f630a3ddc302
 admin.site.register(Account)
