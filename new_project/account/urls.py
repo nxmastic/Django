cefc243e42b8070566b5b91c1423ca1f59377878
nxmastic <48348520+nxmastic@users.noreply.github.com>
2019-07-30 11:50:55 +0900

    conflict error fix

--- new_project/account/urls.py
+++ new_project/account/urls.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 from django.urls import path, include
 from.import views
 
@@ -6,14 +5,3 @@ urlpatterns = [
     # path('',views.LoginForm.as_view(), name='Login')
     path('register',views.register, name='register'),
 ]
-
-=======
-from django.urls import path, include
-from.import views
-
-urlpatterns = [
-    # path('',views.LoginForm.as_view(), name='Login')
-    path('register',views.register, name='register'),
-]
-
->>>>>>> 2480c54b8f7df51e28a370924a66f630a3ddc302
