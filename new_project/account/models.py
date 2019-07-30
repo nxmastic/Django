9796b6c5ed143357bdb8c3ff745754b1e36d57b4
nxmastic <48348520+nxmastic@users.noreply.github.com>
2019-07-30 11:45:06 +0900

    conflict error fix

--- new_project/account/models.py
+++ new_project/account/models.py
@@ -1,21 +1,6 @@
-<<<<<<< HEAD
 from django.db import models
 
 class Account(models.Model):
     userID = models.CharField(max_length=15, unique=True, primary_key=True)
     password = models.CharField(max_length=15)
     nick = models.CharField(max_length=10)
-
-
-# Create your models here.
-=======
-from django.db import models
-
-class Account(models.Model):
-    userID = models.CharField(max_length=15, unique=True, primary_key=True)
-    password = models.CharField(max_length=15)
-    nick = models.CharField(max_length=10)
-
-
-# Create your models here.
->>>>>>> 2480c54b8f7df51e28a370924a66f630a3ddc302
