ea1b8c7ae4d3a7498f6d432aabcb547b5e97388c
nxmastic <48348520+nxmastic@users.noreply.github.com>
2019-07-30 11:48:34 +0900

    conflict error fix

--- new_project/account/apps.py
+++ new_project/account/apps.py
@@ -1,13 +1,5 @@
-<<<<<<< HEAD
 from django.apps import AppConfig
 
 
 class AccountConfig(AppConfig):
     name = 'account'
-=======
-from django.apps import AppConfig
-
-
-class AccountConfig(AppConfig):
-    name = 'account'
->>>>>>> 2480c54b8f7df51e28a370924a66f630a3ddc302
